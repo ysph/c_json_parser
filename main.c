0899259fb44242fda63a14589a3f83dff667015d
ysph <creative.doomsday@gmail.com>
2021-01-03 03:01:02 +0300

    Parser: add string parser
    Now it parses strings of all sorts, at least it works
    with the basic stuff in them and it doesn't crash.

--- main.c
+++ main.c
@@ -1,6 +1,46 @@
 // Copyright Mikhail ysph Subbotin
 #include <stdio.h>
 
+static const int CC_SIZE = 10;
+static const int SC_SIZE = 9;
+static const int CONTROL_CHARS[] = {0, 7, 8, 9, 10, 11, 12, 13, 26, 27};
+static const int SOLIDUS_CHARS[] = {34, 92, 47, 98, 102, 110, 114, 116, 117};
+
+
+int parse_string(FILE *fptr, int *position, int *line) {
+	char ch;
+	PARSE_NEXT:
+	while ((ch = fgetc(fptr)) != EOF) {
+		printf("%c", ch);
+		if (ch == '"') {
+			//printf("\n");
+			return 0;
+		}
+		for (int i = 0; i < CC_SIZE; i++) {
+			if (ch == CONTROL_CHARS[i]) return 'c';
+		}
+		if (ch == '\\') {
+			if ((ch = fgetc(fptr)) == EOF) return EOF;
+			printf("%c", ch);
+			if (ch == SOLIDUS_CHARS[8]) {
+				for (int i = 0; i < 4; i++) {
+					if ((ch = fgetc(fptr)) == EOF) return EOF;
+					printf("%c", ch);
+					if ((ch >= 48 && ch <= 57) || (ch >= 65 && ch <= 70) || (ch >= 97 && ch <= 102)) continue;
+					else {
+						printf("Bad string '%c'\n", ch);
+						return ch;
+					}
+				}
+			}
+			for (int i = 0; i < SC_SIZE - 1; ++i) {
+				if (ch == SOLIDUS_CHARS[i])
+					goto PARSE_NEXT;
+			}
+		}
+	}
+	return EOF;
+}
 int parse_whitespace(FILE *fptr, int *position, int *line) {
 	char ch;
 	while ((ch = fgetc(fptr)) != EOF) {
@@ -27,22 +67,26 @@ int parse_value(FILE *fptr, int *position, int *line) {
 	char ch = parse_whitespace(fptr, position, line);
 	switch (ch) {
 		case '"':
-			puts("parsing string");
+			printf("string = \"");
+			returnCode = parse_string(fptr, position, line);
+			if (returnCode) return returnCode;
 			return parse_whitespace(fptr, position, line);
 		case '{':
 			puts("parsing object");
 			return parse_whitespace(fptr, position, line);
 		case '[':
+			printf("[");
 			returnCode = parse_whitespace(fptr, position, line);
-			if (returnCode == ']')
-				return parse_whitespace(fptr, position, line);
+			if (returnCode == ']') return parse_whitespace(fptr, position, line);
 			fseek(fptr, -1, SEEK_CUR);
 
 			returnCode = parse_value(fptr, position, line);
 			if (returnCode == ']') return returnCode;
 			while (returnCode != ']' && returnCode == ',') {
+				printf(",");
 				returnCode = parse_value(fptr, position, line);
 			}
+			printf("%c",returnCode); // the ']' character
 			if (returnCode != EOF)
 				return parse_whitespace(fptr, position, line);
 
@@ -57,7 +101,7 @@ int parse_value(FILE *fptr, int *position, int *line) {
 					return ch;
 				}
 			}
-			puts("value = true");
+			printf("true");
 			return parse_whitespace(fptr, position, line);
 		case 'f':
 			for (int i = 1; i < 5; ++i) {
@@ -68,7 +112,7 @@ int parse_value(FILE *fptr, int *position, int *line) {
 					return ch;
 				}
 			}
-			puts("value = false");
+			printf("false");
 			return parse_whitespace(fptr, position, line);
 		case 'n':
 			for (int i = 1; i < 4; ++i) {
@@ -79,7 +123,7 @@ int parse_value(FILE *fptr, int *position, int *line) {
 					return ch;
 				}
 			}
-			puts("value = null");
+			printf("null");
 			return parse_whitespace(fptr, position, line);
 		default:
 			printf("Unexpected '%c'\n", ch);
@@ -100,7 +144,7 @@ int parse_json(char *str) {
 
 		//parse_object(fptr, &position, &line);
 		fclose(fptr);
-		printf("\nFile closed.\n");
+		printf("\n\nFile closed.\n");
 		return 0;
 	}
 	return 1;
